902ba97aa650a6db401a8f0838ee721cc409dfbc
Kieran <kieran@keywebdesigns.com>
2021-05-26 12:40:17 -0700

    wip

--- src/LaravelGoogleMaps.php
+++ src/LaravelGoogleMaps.php
@@ -779,6 +779,12 @@ class LaravelGoogleMaps
         ';
 
         $polygon_output .= '
+        
+        /**
+         *  Returns an array of coordinate after editing polygon
+         *  It will despach an event called newCoords, you can watch for this in you code and take action.
+         */
+         
             function getPolygonCoords() {
                 var coordinates_poly = polygon_'.count($this->polygons).'.getPath().getArray();
                 var newCoordinates_poly = [];
@@ -794,6 +800,95 @@ class LaravelGoogleMaps
                 document.dispatchEvent(event);
 
             }
+            
+             /**
+               * A menu that lets a user delete a selected vertex of a path.
+               */
+              class DeleteMenu extends google.maps.OverlayView {
+                constructor() {
+                  super();
+                  this.div_ = document.createElement("div");
+                  this.div_.className = "delete-menu";
+                  this.div_.innerHTML = "Delete";
+                  const menu = this;
+                  google.maps.event.addDomListener(this.div_, "click", () => {
+                    menu.removeVertex();
+                  });
+                }
+                onAdd() {
+                  const deleteMenu = this;
+                  const map = this.getMap();
+                  this.getPanes().floatPane.appendChild(this.div_);
+                  // mousedown anywhere on the map except on the menu div will close the
+                  // menu.
+                  this.divListener_ = google.maps.event.addDomListener(
+                    map.getDiv(),
+                    "mousedown",
+                    (e) => {
+                      if (e.target != deleteMenu.div_) {
+                        deleteMenu.close();
+                      }
+                    },
+                    true
+                  );
+                }
+                onRemove() {
+                  if (this.divListener_) {
+                    google.maps.event.removeListener(this.divListener_);
+                  }
+                  this.div_.parentNode.removeChild(this.div_);
+                  // clean up
+                  this.set("position", null);
+                  this.set("path", null);
+                  this.set("vertex", null);
+                }
+                close() {
+                  this.setMap(null);
+                }
+                draw() {
+                  const position = this.get("position");
+                  const projection = this.getProjection();
+
+                  if (!position || !projection) {
+                    return;
+                  }
+                  const point = projection.fromLatLngToDivPixel(position);
+                  this.div_.style.top = point.y + "px";
+                  this.div_.style.left = point.x + "px";
+                }
+                /**
+                 * Opens the menu at a vertex of a given path.
+                 */
+                open(map, path, vertex) {
+                  this.set("position", path.getAt(vertex));
+                  this.set("path", path);
+                  this.set("vertex", vertex);
+                  this.setMap(map);
+                  this.draw();
+                }
+                /**
+                 * Deletes the vertex from the path.
+                 */
+                removeVertex() {
+                  const path = this.get("path");
+                  const vertex = this.get("vertex");
+
+                  if (!path || vertex == undefined) {
+                    this.close();
+                    return;
+                  }
+                  path.removeAt(vertex);
+                  this.close();
+                }
+              }
+              const deleteMenu = new DeleteMenu();
+              google.maps.event.addListener(polygon_'.count($this->polygons).', "rightclick", (e) => {
+                // Check if click was on a vertex control point
+                if (e.vertex == undefined) {
+                  return;
+                }
+                deleteMenu.open(map, polygon_'.count($this->polygons).'.getPath(), e.vertex);
+              });
 
         ';
 
